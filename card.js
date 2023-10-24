4757196642df9ec4f26c5fdb29724d506b9df7d2
Satyam Kumar <silentvoice4143@gmail.com>
2023-10-24 10:06:50 +0530

    first commit

---
+++ card.js
@@ -0,0 +1,284 @@
+const cards = [
+  {
+    img: "./imgs/cources/istockphoto-1340766096-612x612.jpg",
+    organization: "IBM",
+    logo: "./imgs/logos/images.png",
+    title: "Data Science course",
+    discription: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Perferendis, harum? Debitis ullam harum consequatur doloremque
+        recusandae similique nihil quae quaerat.`,
+    starrating: 4.5,
+    review: 34,
+    level: "Beginner",
+    coursetype: "Specialization",
+    duration: "3-6 Months",
+    subject: "web development",
+  },
+  {
+    img: "./imgs/cources/istockphoto-1340766096-612x612.jpg",
+    organization: "IBM",
+    logo: "./imgs/logos/images.png",
+    title: "Data Science course",
+    discription: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Perferendis, harum? Debitis ullam harum consequatur doloremque
+        recusandae similique nihil quae quaerat.`,
+    starrating: 4.5,
+    review: 34,
+    level: "Beginner",
+    coursetype: "Specialization",
+    duration: "3-6 Months",
+    subject: "web development",
+  },
+  {
+    img: "./imgs/cources/istockphoto-1340766096-612x612.jpg",
+    organization: "IBM",
+    logo: "./imgs/logos/images.png",
+    title: "Data Science course",
+    discription: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Perferendis, harum? Debitis ullam harum consequatur doloremque
+        recusandae similique nihil quae quaerat.`,
+    starrating: 4.5,
+    review: 34,
+    level: "Beginner",
+    coursetype: "Specialization",
+    duration: "3-6 Months",
+    subject: "web development",
+  },
+  {
+    img: "./imgs/cources/istockphoto-1340766096-612x612.jpg",
+    organization: "IBM",
+    logo: "./imgs/logos/images.png",
+    title: "Data Science course",
+    discription: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Perferendis, harum? Debitis ullam harum consequatur doloremque
+        recusandae similique nihil quae quaerat.`,
+    starrating: 4.5,
+    review: 34,
+    level: "Beginner",
+    coursetype: "Specialization",
+    duration: "3-6 Months",
+    subject: "web development",
+  },
+  {
+    img: "./imgs/cources/istockphoto-1340766096-612x612.jpg",
+    organization: "IBM",
+    logo: "./imgs/logos/images.png",
+    title: "Data Science course",
+    discription: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Perferendis, harum? Debitis ullam harum consequatur doloremque
+        recusandae similique nihil quae quaerat.`,
+    starrating: 4.5,
+    review: 34,
+    level: "Beginner",
+    coursetype: "Specialization",
+    duration: "3-6 Months",
+    subject: "web development",
+  },
+  {
+    img: "./imgs/cources/istockphoto-1340766096-612x612.jpg",
+    organization: "IBM",
+    logo: "./imgs/logos/images.png",
+    title: "Data Science course",
+    discription: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Perferendis, harum? Debitis ullam harum consequatur doloremque
+        recusandae similique nihil quae quaerat.`,
+    starrating: 4.5,
+    review: 34,
+    level: "Beginner",
+    coursetype: "Specialization",
+    duration: "3-6 Months",
+    subject: "web development",
+  },
+  {
+    img: "./imgs/cources/istockphoto-1340766096-612x612.jpg",
+    organization: "IBM",
+    logo: "./imgs/logos/images.png",
+    title: "Data Science course",
+    discription: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Perferendis, harum? Debitis ullam harum consequatur doloremque
+        recusandae similique nihil quae quaerat.`,
+    starrating: 4.5,
+    review: 34,
+    level: "Beginner",
+    coursetype: "Specialization",
+    duration: "3-6 Months",
+    subject: "web development",
+  },
+  {
+    img: "./imgs/cources/istockphoto-1340766096-612x612.jpg",
+    organization: "IBM",
+    logo: "./imgs/logos/images.png",
+    title: "Data Science course",
+    discription: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Perferendis, harum? Debitis ullam harum consequatur doloremque
+        recusandae similique nihil quae quaerat.`,
+    starrating: 4.5,
+    review: 34,
+    level: "Beginner",
+    coursetype: "Specialization",
+    duration: "3-6 Months",
+    subject: "web development",
+  },
+  {
+    img: "./imgs/cources/istockphoto-1340766096-612x612.jpg",
+    organization: "IBM",
+    logo: "./imgs/logos/images.png",
+    title: "Data Science course",
+    discription: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Perferendis, harum? Debitis ullam harum consequatur doloremque
+        recusandae similique nihil quae quaerat.`,
+    starrating: 4.5,
+    review: 32,
+    level: "Beginner",
+    coursetype: "Specialization",
+    duration: "3-6 Months",
+    subject: "web development",
+  },
+  {
+    img: "./imgs/cources/istockphoto-1340766096-612x612.jpg",
+    organization: "IBM",
+    logo: "./imgs/logos/images.png",
+    title: "Data Science course",
+    discription: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Perferendis, harum? Debitis ullam harum consequatur doloremque
+        recusandae similique nihil quae quaerat.`,
+    starrating: 4.5,
+    review: 34,
+    level: "Beginner",
+    coursetype: "Specialization",
+    duration: "3-6 Months",
+    subject: "web development",
+  },
+  {
+    img: "./imgs/cources/istockphoto-1340766096-612x612.jpg",
+    organization: "IBM",
+    logo: "./imgs/logos/images.png",
+    title: "Data Science course",
+    discription: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Perferendis, harum? Debitis ullam harum consequatur doloremque
+        recusandae similique nihil quae quaerat.`,
+    starrating: 4.5,
+    review: 34,
+    level: "Beginner",
+    coursetype: "Specialization",
+    duration: "3-6 Months",
+    subject: "business",
+  },
+  {
+    img: "./imgs/cources/istockphoto-1340766096-612x612.jpg",
+    organization: "IBM",
+    logo: "./imgs/logos/images.png",
+    title: "Data Science course",
+    discription: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Perferendis, harum? Debitis ullam harum consequatur doloremque
+        recusandae similique nihil quae quaerat.`,
+    starrating: 4.5,
+    review: 34,
+    level: "Beginner",
+    coursetype: "Specialization",
+    duration: "3-6 Months",
+    subject: "business",
+  },
+];
+
+var createdcards = [];
+
+const cards_wrapper = $(".cards-wrapper");
+
+function cardFunction() {
+  cards.forEach(createCard);
+  // console.log(createdcards);
+}
+
+function createCard(e) {
+  var subject = e.subject.toLowerCase();
+  subject = subject.replace(/ /g, "");
+  var duration = e.duration.toLowerCase();
+  duration = duration.replace(/ /g, "");
+  // console.log(subject);
+  // console.log(duration);
+  var card = `<div class="card ${subject} ${duration}">
+    <div class="card-img">
+      <img
+        src=${e.img}
+        alt=""
+      />
+    </div>
+    <div class="card-body">
+      <div class="card-company">
+        <img src=${e.logo} alt="" />
+        <div class="organization">IBM</div>
+      </div>
+      <h4 class="card-title">${e.title}</h4>
+      <p class="card-para">
+                  <p class="skills">Skills you'll gain: </p>
+                  ${e.discription}
+      </p>
+      <div class="features-wrapper">
+        <div class="card-rate_review">
+          <div class="card-rating">
+            <i class="fa fa-star"></i>
+            <p class="star">${e.starrating}</p>
+          </div>
+          <div class="card-review">(${e.review}k reviews)</div>
+        </div>
+        <div class="course-features">
+          <ul>
+            <li><span>.</span>${e.level}</li>
+            <li><span>.</span>${e.coursetype}</li>
+            <li><span>.</span>${e.duration}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>`;
+  cards_wrapper.append(card);
+  createdcards.push(card);
+  //   console.log(card);
+}
+cardFunction();
+
+function change() {
+  console.log("running change function");
+  let results = Array.from(document.querySelectorAll(".cards-wrapper > div"));
+  // let results = createdcards;
+  // console.log(results);
+  let subjectChecked = $(".filters-wrapper input.subject:checked"),
+    durationChecked = $(".filters-wrapper input.duration:checked");
+  // Hide all results
+  results.forEach(function (result) {
+    result.style.display = "none";
+  });
+  // Filter results to only those that meet ALL requirements:
+  // console.log(durationChecked);
+  if (subjectChecked.length != 0) {
+    filterRequirements(subjectChecked);
+  }
+
+  if (durationChecked.length != 0) {
+    filterRequirements(durationChecked);
+  }
+
+  // if (durationChecked.length === 0 && subjectChecked.length === 0) {
+  //   cardFunction();
+  //   return;
+  // }
+
+  function filterRequirements(checkedelement) {
+    results = Array.from(checkedelement).reduce(function (sum, input) {
+      const attrib = input.getAttribute("name");
+      console.log(attrib);
+      return sum.concat(
+        results.filter(function (result) {
+          console.log(result.classList);
+          return result.classList.contains(attrib);
+        })
+      );
+    }, []);
+  }
+  // Show those filtered results:
+  results.forEach(function (result) {
+    console.log(result);
+    result.style.display = "block";
+  });
+}
